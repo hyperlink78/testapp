c1bba896a0d0242cb7879a584b830dbfd30c6501
hyperlink78 <hyperlinkfootball@mail.ru>
2023-06-23 10:46:07 +0300

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Денис Казаринов on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
